677b157d660a9a61d517a21bc6507eb19cd50f86
satyapatel293 <satyapatel293@gmail.com>
2022-11-09 10:55:25 -0500

    Complete calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,49 +1,111 @@
-# ask for tow numbers
-# ask the user for an operation to perfrom 
-# perfrom the operation on the two numbers
-# output the result
+def prompt(message)
+  puts "=> #{message}"
+end
 
-# answer = Kernel.gets() 
-# Kernel.puts(answer)
+def integer?(num)
+  num.to_i.to_s == num
+end
 
-Kernel.puts("Welcome to Calculator!")
+def float?(num)
+  num.to_f.to_s == num
+end
 
-Kernel.puts("What's the first number?")
-number1 = gets().chomp()
+def number?(num)
+  integer?(num) || float?(num)
+end
 
-Kernel.puts("What's the second number?")
-number2= gets().chomp()
+def operation_to_message(op)
+  message = case op
+            when "1"
+              "Adding"
+            when '2'
+              "Subtracting"
+            when "3"
+              "Multiplying"
+            when '4'
+              "Dividing"
+            end
+   a = 'something'
+  operation
+end
 
-Kernel.puts("What opperation would you like to perfrom? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
+prompt("Welcome to Calculator! Enter your name:")
+name = ''
+loop do
+  name = gets.chomp
 
-if operator == "1"
-  result = number1.to_i() + number2.to_i()
-elsif operator == "2"
-  result = number1.to_i() - number2.to_i()
-elsif operator == "3"
-  result = number1.to_i() * number2.to_i()
-else 
-  # must convert to float in this case because int division always produces an int
-  # makes sense on when not to convert string to in immediatly when gets() inputs
-  result = number1.to_f() / number2.to_f()
-end 
+  if name.empty?
+    prompt "Make sure to use a valid name."
+  else
+    break
+  end
+end
 
-Kernel.puts("The result is #{result}")
+prompt "Hi #{name}!"
 
+loop do # main loop
+  number1 = ''
+  loop do
+    prompt "What's the first number?"
+    number1 = gets.chomp
 
+    if number?(number1)
+      break
+    else
+      prompt "Hmm.. that doesn't look like a valid number"
+    end
+  end
 
-# puts ">> First number?"
-# num1 = gets.chomp.to_i
-# puts ">> Second number?"
-# num2 = gets.chomp.to_i
-# puts ">> Opperation? (+, -, /, *)"
-# opperation = gets.chomp
+  number2 = ''
+  loop do
+    prompt "What's the second number?"
+    number2 = gets.chomp
 
-# case opperation
-# when '+' then puts "#{num1} + #{num2} = #{num1 + num2}"
-# when '-' then puts "#{num1} - #{num2} = #{num1 - num2}"
-# when '*' then puts "#{num1} * #{num2} = #{num1 * num2}"
-# when '/' then puts "#{num1} / #{num2} = #{num1 / num2}"
-# end
+    if number?(number2)
+      break
+    else
+      prompt "Hmm.. that doesn't look like a valid number"
+    end
+  end
 
+  operator_prompt = <<-MSG
+    What opperation would you like to perfrom? 
+    1) add 
+    2) subtract 
+    3) multiply 
+    4) divide
+  MSG
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3 or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when "1"
+             number1.to_i + number2.to_i
+           when "2"
+             number1.to_i - number2.to_i
+           when "3"
+             number1.to_i * number2.to_i
+           when "4"
+             number1.to_f / number2.to_f
+           end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perform another calculation?(Y to calculate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Good bye!")
---
+++ lesson_2/notes_calculator.md
@@ -0,0 +1,21 @@
+
+#LOGIC
+# ask for tow numbers
+# ask the user for an operation to perfrom 
+# perfrom the operation on the two numbers
+# output the result
+
+# answer = Kernel.gets() 
+# Kernel.puts(answer)
+
+# if operator == "1"
+#   result = number1.to_i() + number2.to_i()
+# elsif operator == "2"
+#   result = number1.to_i() - number2.to_i()
+# elsif operator == "3"
+#   result = number1.to_i() * number2.to_i()
+# else 
+#   # must convert to float in this case because int division always produces an int
+#   # makes sense on when not to convert string to in immediatly when gets() inputs
+#   result = number1.to_f() / number2.to_f()
+# end 
