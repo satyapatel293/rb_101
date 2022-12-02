d38382c76bb7de0d83d2028bc16068cff29783c6
satyapatel293 <satyapatel293@gmail.com>
2022-12-01 22:00:05 -0500

    Complete up to part 5 of tictactoe bonus

--- lesson_6/bonus_tictactoe.rb
+++ lesson_6/bonus_tictactoe.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 WINNIG_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                [[1, 5, 9], [3, 5, 7]]              # diags
@@ -13,24 +11,43 @@ def prompt(msg)
 end
 
 # rubocop: disable Metrics/AbcSize
-def display_board(brd)
+def display_board(brd, score, round) #TODO make board nicer and switch to large messafe format
   system "clear"
-  puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
-  puts ""
-  puts "     |     |"
-  puts "  #{brd[1]}  |  #{brd[2]}  |   #{brd[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "  #{brd[4]}  |  #{brd[5]}  |   #{brd[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "  #{brd[7]}  |  #{brd[8]}  |   #{brd[9]}"
-  puts "     |     |"
-  puts ""
+  puts "         Current Score"
+  puts "Player ['X']: #{score[:player_score]} | Computer['O']: #{score[:computer_score]}"
+  puts "            Round #{round}"
+  puts ' '
+  puts "            |     |"
+  puts "         #{brd[1]}  |  #{brd[2]}  |   #{brd[3]}"
+  puts "            |     |"
+  puts "       -----+-----+-----"
+  puts "            |     |"
+  puts "         #{brd[4]}  |  #{brd[5]}  |   #{brd[6]}"
+  puts "            |     |"
+  puts "       -----+-----+-----"
+  puts "            |     |"
+  puts "         #{brd[7]}  |  #{brd[8]}  |   #{brd[9]}"
+  puts "            |     |"
+  puts " "
 end
 # rubocop: enable Metrics/AbcSize
+
+def intro_message
+  system 'clear'
+  # prompt "Welcome to Tic Tac Toe!"
+  puts "                                                                                
+  ████████╗██╗ ██████╗████████╗ █████╗  ██████╗████████╗ ██████╗ ███████╗
+  ╚══██╔══╝██║██╔════╝╚══██╔══╝██╔══██╗██╔════╝╚══██╔══╝██╔═══██╗██╔════╝
+     ██║   ██║██║        ██║   ███████║██║        ██║   ██║   ██║█████╗  
+     ██║   ██║██║        ██║   ██╔══██║██║        ██║   ██║   ██║██╔══╝  
+     ██║   ██║╚██████╗   ██║   ██║  ██║╚██████╗   ██║   ╚██████╔╝███████╗
+     ╚═╝   ╚═╝ ╚═════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚══════╝                        
+  "
+  puts 'Each cell has a number from 1-9 like this '
+   #TODO add matrix of avalib
+  puts 'First player to win 5 rounds wins the game'
+  sleep(4)
+end
 
 def initalize_board
   new_board = {}
@@ -42,10 +59,21 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(array, delimiter=', ', last_word="or")
+  case array.length
+  when 0 then ''
+  when 1 then array.first.to_s
+  when 2 then array.join(" #{last_word} ")
+  else 
+    array[-1] = "#{last_word} #{array[-1]}"
+    array.join(delimiter)
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Chose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Chose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry that's not a vaild choice."
@@ -54,8 +82,33 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  attacking_move = detect_near_win(brd, COMPUTER_MARKER)
+  defensive_move = detect_near_win(brd, PLAYER_MARKER)
+  
+  square = if attacking_move 
+             attacking_move
+           elsif defensive_move 
+             defensive_move 
+           elsif brd[5] == INITIAL_MARKER
+             5
+           else
+             empty_squares(brd).sample
+           end
+
   brd[square] = COMPUTER_MARKER
+end
+
+def detect_near_win(brd, marker)
+  move = nil 
+  WINNIG_LINES.each do |line|
+    markers_on_line = brd.values_at(*line)
+    if markers_on_line.count(marker) == 2 && 
+       markers_on_line.count(INITIAL_MARKER) == 1
+      empty_marker_on_line = empty_squares(brd).select { |num| line.include?(num)}.first
+      move = empty_marker_on_line
+    end
+  end
+  move
 end
 
 def board_full?(brd)
@@ -68,42 +121,58 @@ end
 
 def detect_winner(brd)
   WINNIG_LINES.each do |line|
-    # if brd[line[0]] == PLAYER_MARKER &&
-    #    brd[line[1]] == PLAYER_MARKER &&
-    #    brd[line[2]] == PLAYER_MARKER
-    #   return "Player"
-    # elsif brd[line[0]] == COMPUTER_MARKER &&
-    #       brd[line[1]] == COMPUTER_MARKER &&
-    #       brd[line[2]] == COMPUTER_MARKER
-    #   return "Computer"
-    # end
-    if brd.values_at(line[0], line[1], line[2]).count("X") == 3
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return "Player"
-    elsif brd.values_at(line[0], line[1], line[2]).count("O") == 3
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return "Computer"
     end
   end
   nil
 end
 
-loop do
-  board = initalize_board
+def update_score!(brd, score)
+  case detect_winner(brd)
+  when "Player" then score[:player_score] += 1
+  when "Computer" then score[:computer_score] += 1
+  else nil 
+  end 
+end 
 
-  loop do
-    display_board(board)
+loop do #main loop
+  intro_message
+  score = {player_score: 0, computer_score: 0} # reset scores each game
+  round = 1
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+  loop do #game loop
+    board = initalize_board #reset board each round
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-  end
+    loop do #round loop
+      display_board(board, score, round)
 
-  display_board(board)
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
+      player_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
+    update_score!(board, score)
+    display_board(board, score, round)
+    
+    if someone_won?(board)
+      prompt "Round #{detect_winner(board)}!"
+    else
+      prompt "It's a tie!"
+    end
+
+    if score.values.include?(5)
+      prompt "#{detect_winner(board).upcase} WON THE GAME!" #TODO add winning Screen!
+      break
+    end
+
+    round += 1
+    prompt "wait for next round..."
+    sleep(1.5)
   end
 
   prompt "Play agian? (y or n)"
@@ -112,3 +181,6 @@ loop do
 end
 
 prompt "Thanks for playing Tic Tac Toe! Good bye!"
+
+
+
---
+++ study_sessions/spot_notes_11_30.rb
@@ -0,0 +1,35 @@
+# variables as pointers 
+# specifically related to arrays 
+
+
+the object refernenced by local variable name 
+vs the method parameter name 
+
+
+
+=begin
+
+# How to access values of colors? 
+The `hsh` is composed of keys representing fruits and vegetables. The keys are paired with values, which represent characteristics of each fruit/vegetable as a nested hash. To access the colors of a particular fruit/veg, we reference the key for that f/v in the outer hash, which allows us to access the inner characteristics hash, and then we reference the `colors` key, which is associated with an array value containing the colors strings.
+
+# Start with concrete. 
+# To access the color strings associted with 'grape'. 
+  We'd reference the `grape` key of `hsh`, which returns an inner hash, `inner_hsh`. Then, we'd reference `colors` key of `inner_hsh`, which returns an array of the color strings. 
+
+
+
+################ BETTER #################
+We're working with a nested hash object that contains keys representing fruits or vegetables. Each of those keys references an inner hash as its value; within the inner hash there are key value pairs representing characteristics of each fruit or vegetable. The keys are symbols, and they reference either strings or arrays containing the specific characteristics of that fruit/veg.
+
+The `#map` method invoked on the hash object referenced by `hsh` and we're assigning each key, value pair to the block parameters `key` and `inner_hash`.
+
+Reference the :type key within the hash referenced by local block variable `inner_hash` and this will return a string with the value "fruit" or "vegetable"
+
+We initialize a local variable `array` to the return value of invoking `map` on the array referenced by `hsh`. `map` iterates over each key-value pair in the `hsh`. On lines 58-66, we pass in a do..end block to `map`. This outer-level `map` invocation will transform each key-value pair into the return value of the block on each iteration and return an array containing these element. 
+  
+
+  On each iteration, we assign the current value to the local variable `inner_hash`. If the value associated with `inner_hash[:type]` is equal to the String `fruit`, then we'll invoke `map` on `inner_hash[:colors]` and pass in a block. Otherwise, the key-value pair will be transformed into `nil`. 
+
+
+
+=end
