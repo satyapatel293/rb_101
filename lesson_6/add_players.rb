d73d4ac2faf3bf1ab60a9a6c9bd4b0ea65aac6a0
satyapatel293 <satyapatel293@gmail.com>
2022-12-03 17:14:39 -0500

    Create temp file to hold current tictactoe program

--- lesson_6/add_players.rb
+++ lesson_6/add_players.rb
@@ -9,6 +9,40 @@ BANNER = <<~BNR
    ╚═╝   ╚═╝ ╚═════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚══════╝ 
 BNR
 
+WIN_COMPUTER = <<~COM
+██████╗ ██████╗ ███╗   ███╗██████╗ ██╗   ██╗████████╗███████╗██████╗     
+██╔════╝██╔═══██╗████╗ ████║██╔══██╗██║   ██║╚══██╔══╝██╔════╝██╔══██╗    
+██║     ██║   ██║██╔████╔██║██████╔╝██║   ██║   ██║   █████╗  ██████╔╝    
+██║     ██║   ██║██║╚██╔╝██║██╔═══╝ ██║   ██║   ██║   ██╔══╝  ██╔══██╗    
+╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ╚██████╔╝   ██║   ███████╗██║  ██║    
+ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝      ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝    
+                                                                          
+            ██╗    ██╗██╗███╗   ██╗███████╗██╗██╗██╗██╗██╗                
+            ██║    ██║██║████╗  ██║██╔════╝██║██║██║██║██║                
+            ██║ █╗ ██║██║██╔██╗ ██║███████╗██║██║██║██║██║                
+            ██║███╗██║██║██║╚██╗██║╚════██║╚═╝╚═╝╚═╝╚═╝╚═╝                
+            ╚███╔███╔╝██║██║ ╚████║███████║██╗██╗██╗██╗██╗                
+             ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝╚═╝╚═╝╚═╝╚═╝                
+
+COM
+
+WIN_PLAYER = <<~PLR
+  ██████╗ ██╗      █████╗ ██╗   ██╗███████╗██████╗     
+  ██╔══██╗██║     ██╔══██╗╚██╗ ██╔╝██╔════╝██╔══██╗    
+  ██████╔╝██║     ███████║ ╚████╔╝ █████╗  ██████╔╝    
+  ██╔═══╝ ██║     ██╔══██║  ╚██╔╝  ██╔══╝  ██╔══██╗    
+  ██║     ███████╗██║  ██║   ██║   ███████╗██║  ██║    
+  ╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝    
+                                                      
+      ██╗    ██╗██╗███╗   ██╗███████╗██╗██╗██╗         
+      ██║    ██║██║████╗  ██║██╔════╝██║██║██║         
+      ██║ █╗ ██║██║██╔██╗ ██║███████╗██║██║██║         
+      ██║███╗██║██║██║╚██╗██║╚════██║╚═╝╚═╝╚═╝         
+      ╚███╔███╔╝██║██║ ╚████║███████║██╗██╗██╗         
+       ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝╚═╝╚═╝         
+
+PLR
+
 WINNIG_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                [[1, 5, 9], [3, 5, 7]]              # diags
@@ -21,34 +55,32 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-# rubocop: disable Metrics/AbcSize
-def display_board(brd, score, round) #TODO make board nicer and switch to large messafe format
+def display_board(brd, score, round)
   system "clear"
-  puts <<~MSG          
-            Current Score
-   Player ['X']: #{score[:player_score]} | Computer['O']: #{score[:computer_score]}
-
-               Round #{round}
-            
-               |     |
-            #{brd[1]}  |  #{brd[2]}  |   #{brd[3]}
-               |     |
-          -----+-----+-----
-               |     |
-            #{brd[4]}  |  #{brd[5]}  |   #{brd[6]}
-               |     |
-          -----+-----+-----
-               |     |
-            #{brd[7]}  |  #{brd[8]}  |   #{brd[9]}
-               |     |
-  
-  MSG
+  puts <<~MSG
+     _____________________________________
+    |            TIC-TAC-TOE              |
+    |  X Player   #{score[:player_score]}          Round: #{round}     |      
+    |  O Computer #{score[:computer_score]}                       |    
+    \\_____________________________________/  
+      |  _____________________________  |
+      | | First to 5 wins             | |
+      | |                             | |
+      | |         #{brd[1]} | #{brd[2]} | #{brd[3]}           | |
+      | |        -----------          | |
+      | |         #{brd[4]} | #{brd[5]} | #{brd[6]}           | |
+      | |        -----------          | |
+      | |         #{brd[7]} | #{brd[8]} | #{brd[9]}           | |
+      | |_____________________________| |
+      |_________________________________|
+     /         +           *  *          \\
+    /_____________________________________\\
+MSG
 end
-# rubocop: enable Metrics/AbcSize
 
 def intro_message
   system 'clear'
-  puts <<~MSG 
+  puts <<~MSG
 
   #{BANNER}                     
   
@@ -64,9 +96,37 @@ def intro_message
 
 
   MSG
-  prompt "Press enter to play!"
-  gets
 end
+
+# rubocop: disable Metrics/AbcSize, Metrics/MethodLength
+def who_goes_first
+  choices = { '1' => 'Player', '2' => 'Computer' }
+  decider = nil
+  answer = nil
+
+  prompt "Who decides who goes first?"
+  prompt "1) Player or 2) Computer"
+  loop do
+    decider = gets.chomp
+    break if choices.keys.include?(decider)
+    prompt "Invalid input.. please enter 1 for player or 2 for computer"
+  end
+
+  if decider == '1'
+    prompt "Who do you want to go first?"
+    prompt "1) Player or 2) Computer"
+    loop do
+      answer = gets.chomp
+      break if choices.keys.include?(decider)
+      prompt "Invalid input.. please enter 1 for player or 2 for computer"
+    end
+  else
+    answer = %w(1 2).sample
+  end
+
+  choices[answer]
+end
+# rubocop: enable Metrics/AbcSize, Metrics/MethodLength
 
 def initalize_board
   new_board = {}
@@ -83,7 +143,7 @@ def joinor(array, delimiter=', ', last_word="or")
   when 0 then ''
   when 1 then array.first.to_s
   when 2 then array.join(" #{last_word} ")
-  else 
+  else
     array[-1] = "#{last_word} #{array[-1]}"
     array.join(delimiter)
   end
@@ -103,11 +163,11 @@ end
 def computer_places_piece!(brd)
   attacking_move = detect_near_win(brd, COMPUTER_MARKER)
   defensive_move = detect_near_win(brd, PLAYER_MARKER)
-  
-  square = if attacking_move 
+
+  square = if attacking_move
              attacking_move
-           elsif defensive_move 
-             defensive_move 
+           elsif defensive_move
+             defensive_move
            elsif brd[5] == INITIAL_MARKER
              5
            else
@@ -118,13 +178,12 @@ def computer_places_piece!(brd)
 end
 
 def detect_near_win(brd, marker)
-  move = nil 
+  move = nil
   WINNIG_LINES.each do |line|
     markers_on_line = brd.values_at(*line)
-    if markers_on_line.count(marker) == 2 && 
+    if markers_on_line.count(marker) == 2 &&
        markers_on_line.count(INITIAL_MARKER) == 1
-      empty_marker_on_line = empty_squares(brd).select { |num| line.include?(num)}.first
-      move = empty_marker_on_line
+      move = empty_squares(brd).select { |v| line.include?(v) }.first
     end
   end
   move
@@ -148,74 +207,52 @@ def detect_winner(brd)
   end
   nil
 end
-  
+
 def update_score!(brd, score)
   case detect_winner(brd)
   when "Player" then score[:player_score] += 1
   when "Computer" then score[:computer_score] += 1
-  else nil 
-  end 
-end 
-
-
-def who_goes_first
-  prompt "Who decides who goes first?"
-  puts "1) Player or 2) computer"
-  decider = nil
-  loop do 
-    decider = gets.chomp
-    break if decider == '1' || decider == "2"
-    prompt "Invalid input.. please enter 1 for player or 2 for computer"
   end
+end
 
-  if decider == '1'
-    prompt "Who do you want to go first?"
-    puts "1) Player or 2) computer"
-    answer = nil
-    loop do 
-      answer = gets.chomp
-      break if answer == '1' || answer == "2"
-      prompt "Invalid input.. please enter 1 for player or 2 for computer"
-    end
+def place_piece!(brd, player)
+  player == "Player" ? player_places_piece!(brd) : computer_places_piece!(brd)
+end
+
+def alternate_player(player)
+  player == "Player" ? "Computer" : "Player"
+end
+
+def winning_screen(brd, score)
+  system "clear"
+  if detect_winner(brd) == "Computer"
+    puts WIN_COMPUTER
   else
-    answer = %w(1 2).sample
+    puts WIN_PLAYER
   end
-  
-  case answer
-  when "1" then "Player"
-  when "2" then "Computer"
-  end
+  prompt "Player: #{score[:player_score]} | Computer: #{score[:computer_score]} "
 end
 
-def place_piece!(brd, current_ply)
-  current_ply == "Player" ? player_places_piece!(brd) : computer_places_piece!(brd)
-end
-
-def alternate_player(current_ply)
-  current_ply == "Player"? current_ply = "Computer" : current_ply = "Player"
-end
-
-loop do #main loop
+loop do # main loop
   intro_message
-  score = {player_score: 0, computer_score: 0} # reset scores each game
+  score = { player_score: 0, computer_score: 0 } # reset scores each game
   round = 1
-  current_player = who_goes_first
+  first_move = who_goes_first
 
-  loop do #game loop
-    board = initalize_board #reset board each round
+  loop do # game loop
+    board = initalize_board # reset board each round
+    current_player = first_move
 
-    loop do #round loop
-      loop do
-        display_board(board, score, round)
-        place_piece!(board, current_player)
-        current_player = alternate_player(current_player)
-        break if someone_won?(board) || board_full?(board)
-      end
+    loop do # round loop
+      display_board(board, score, round)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
+      break if someone_won?(board) || board_full?(board)
     end
 
     update_score!(board, score)
     display_board(board, score, round)
-    
+
     if someone_won?(board)
       prompt "Round #{detect_winner(board)}!"
     else
@@ -223,7 +260,7 @@ loop do #main loop
     end
 
     if score.values.include?(5)
-      prompt "#{detect_winner(board).upcase} WON THE GAME!" #TODO add winning Screen!
+      winning_screen(board, score)
       break
     end
 
@@ -238,4 +275,3 @@ loop do #main loop
 end
 
 prompt "Thanks for playing Tic Tac Toe! Good bye!"
-
